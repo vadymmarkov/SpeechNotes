00d95831eea72e3b288300d37ca0b58d3d4ee6fa
Vadym Markov <markov.vadym@gmail.com>
2018-01-09 22:58:33 +0100

    Initial Commit

---
+++ SpeechNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechNotes
+//
+//  Created by Vadym Markov on 09/01/2018.
+//  Copyright © 2018 Vadym Markov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
